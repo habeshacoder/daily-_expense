ab691839e3eaa82a98c5e181b43fc735293a9f4e
ftimedev <ftimedev@gmail.com>
2024-10-20 02:47:48 +0300

    Update expense_prover.dart

--- lib/provider/expense_prover.dart
+++ lib/provider/expense_prover.dart
@@ -1,1 +1,8 @@
-class TransactionNotifier {}
+class TransactionNotifier extemd ChangeNotifier{
+  //to do: add the fetching logic here when the user opens the app
+
+  
+  //to do: add the putting hive objects here when the user adds a new expense
+
+  //to do: better to add the search and filter functionalities here too!
+}
